0890120493b3bd1d3396990a6f947eea66b260d7
finde <finde.xumara@gmail.com>
2015-05-18 10:35:16 +0200

    fixed unicode issue

--- data_prep.py
+++ data_prep.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python2
 
+
+import sys
 import nltk
 from itertools import izip
-import codecs
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
 
 def loaddata(sourceF, targetF, outFile):
     toker = nltk.tokenize.RegexpTokenizer('\w+|\S+')
 
     sents = []
-    out = open(outFile,'w')
+    out = open(outFile, 'w')
     with open(sourceF) as sF, open(targetF) as tF:
-        for s,t in izip(sF,tF):
+        for s, t in izip(sF, tF):
             if len(s) < 40 and len(t) < 40:
-                for w in toker.tokenize(s):
-                    out.write(w.lower().+" ")
+                for w in toker.tokenize(s.decode('utf8')):
+                    out.write(w.lower() + ' ')
                 out.write("||| ")
-                for w in toker.tokenize(t):
-                    out.write(w.lower()+" ")
+                for w in toker.tokenize(t.decode('utf8')):
+                    out.write(w.lower() + ' ')
                 out.write("\n")
-                
-if __name__=="__main__":
-    loaddata("data/train.es","data/train.en","data/train")
-        
+
+
+if __name__ == "__main__":
+    loaddata("data/train.es", "data/train.en", "data/train")
