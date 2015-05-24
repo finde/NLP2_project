112e0cb5714067c31019da944859bc78d96ef2e9
jacob5677688 <jacob.verdegaal@student.uva.nl>
2015-05-24 15:12:42 +0200

    atools doesn't work on arch (boostlibs problems)

---
+++ dataPrep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+paste -d '|' data/train.en data/train.de > tmp
+cat tmp | sed -r 's/\|/ ||| /g' > data/train.en-de
+./cdec-2014-10-12/corpus/tokenize-anything.sh < data/train.en-de | ./cdec-2014-10-12/corpus/lowercase.pl > data/train.tok.en-de
+./cdec-2014-10-12/corpus/filter-length.pl -40 data/train.toc.en-de > data/training.en-de
+
+./fast_align-master/fast_align -i data/training.en-de -d -v -o > data/training.en-de.fwd_align
+./fast_align-master/fast_align -i data/training.en-de -d -v -o -r > data/training.en-de.rev_align
+
+./cdec-2014-10-12/utils/atools -i data/training.en-de.fwd_align -j data/training.en-de.rev_align -c grow-diag-final-and > data/training.gdfa
