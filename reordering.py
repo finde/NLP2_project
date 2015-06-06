a714ad45d51aeff7e4bf4b6ed02405d13698dc04
jacob5677688 <jacob.verdegaal@student.uva.nl>
2015-06-06 18:33:19 +0200

    reference positions from alignments now correctly implemented

--- reordering.py
+++ reordering.py
@@ -3,34 +3,69 @@ from random import shuffle
 from itertools import izip
 from operator import itemgetter
 from nltk import tokenize
+from collections import deque
 
+"""
 def train(sourceF,aligns):
     toker = tokenize.RegexpTokenizer('\w+|\S+')
     with open(sourceF) as srcF, open(aligns) as srcRef:
         for g,gAl in izip(srcF, srcRef):
-            gP = permute(toker.tokenize(g.decode('utf8'))) # according to ITG neigbor
-            gRef = getReference(g,gAl)
+            gToks = toker.tokenize(g.decode('utf8'))
+            gP = permute(gToks) # according to ITG neigbor
+            gRef = getReference(gToks,gAl)
+"""
 
-def permute(gerS):
-    shuffled = np.arange(len(gerS))
-    shuffle(shuffled)
-    return [gerS[i] for i in shuffled]
-
-def getReference(g, gAl):
-    ltoker = tokenize.RegexpTokenizer('\d-\d')
-    ptoker = tokenize.RegexpTokenizer('\d')
-    gRef = []
-    for al in ltoker.tokenize(gAl):
-        pair = ptoker.tokenize(al)
-        gRef.append(g[int(pair[1])])
-
-    print "-----\n"
-    print g
-    print gRef
+def train(toksF,aligns):
+    toker = tokenize.RegexpTokenizer('\w+|\S+')
+    i = 0
+    with open(toksF) as srcF, open(aligns) as srcRef:
+        for toks, refAlign in izip(srcF, srcRef):
+            src, tar = getSents(toker.tokenize(toks))
+            srcP = permute(src) # according to ITG neigbor
+            refOrder = getReference(src,refAlign)
+            i += 1
+            if i > 5:
+                quit()
+            
+def getSents(toks):
+    i = toks.index('|||')
+    return toks[:i], toks[i+1:]
     
-    return gRef
+def permute(srcS):
+    shuffled = np.arange(len(srcS))
+    shuffle(shuffled)
+    return [srcS[i] for i in shuffled]
+
+def getReference(src, alignsS):
+    ltoker = tokenize.RegexpTokenizer('\d+-\d+')
+    ptoker = tokenize.RegexpTokenizer('\d+')
+    srcPos = []
+    aligns = deque([ptoker.tokenize(al) for al in ltoker.tokenize(alignsS)])
+
+    print "-----"
+    print aligns
+    try:
+        while True:
+            al = aligns.popleft()
+            srcPos.append((src[int(al[0])],int(al[1])))
+    except IndexError:
+        check = [p[0] for p in srcPos]
+        for s in src:
+            if s not in check:
+                srcPos.append((s,0))
+                        
+    srcPos.sort(key=itemgetter(1))
+
+    print src
+    #print srcPos
+    #print 'src len:', len(src)
+    refOrder = [p[0] for p in srcPos]
+  
+    print refOrder
+    return refOrder
+
 if __name__ == "__main__":
-    train("data/train.de", "data/training.gdfa")
+    train("data/train.tok.en-de", "data/training.gdfa")
 
 """
 def train():
