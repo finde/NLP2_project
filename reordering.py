1fa19433116e522ce233dd00f238e8094b795a0c
finde <finde.xumara@gmail.com>
2015-06-07 11:33:26 +0200

    refactored + updated gitignore

--- reordering.py
+++ reordering.py
@@ -15,26 +15,30 @@ def train(sourceF,aligns):
             gRef = getReference(gToks,gAl)
 """
 
-def train(toksF,aligns):
+
+def train(toksF, aligns):
     toker = tokenize.RegexpTokenizer('\w+|\S+')
     i = 0
     with open(toksF) as srcF, open(aligns) as srcRef:
         for toks, refAlign in izip(srcF, srcRef):
             src, tar = getSents(toker.tokenize(toks))
-            srcP = permute(src) # according to ITG neigbor
-            refOrder = getReference(src,refAlign)
+            srcP = permute(src)  # according to ITG neigbor
+            refOrder = getReference(src, refAlign)
             i += 1
             if i > 5:
                 quit()
-            
+
+
 def getSents(toks):
     i = toks.index('|||')
-    return toks[:i], toks[i+1:]
-    
+    return toks[:i], toks[i + 1:]
+
+
 def permute(srcS):
     shuffled = np.arange(len(srcS))
     shuffle(shuffled)
     return [srcS[i] for i in shuffled]
+
 
 def getReference(src, alignsS):
     ltoker = tokenize.RegexpTokenizer('\d+-\d+')
@@ -47,22 +51,23 @@ def getReference(src, alignsS):
     try:
         while True:
             al = aligns.popleft()
-            srcPos.append((src[int(al[0])],int(al[1])))
+            srcPos.append((src[int(al[0])], int(al[1])))
     except IndexError:
         check = [p[0] for p in srcPos]
         for s in src:
             if s not in check:
-                srcPos.append((s,0))
-                        
+                srcPos.append((s, 0))
+
     srcPos.sort(key=itemgetter(1))
 
     print src
-    #print srcPos
-    #print 'src len:', len(src)
+    # print srcPos
+    # print 'src len:', len(src)
     refOrder = [p[0] for p in srcPos]
-  
+
     print refOrder
     return refOrder
+
 
 if __name__ == "__main__":
     train("data/train.tok.en-de", "data/training.gdfa")
