de820105ac3ecc4e5967a4d2c530c4a4261c0316
finde <finde.xumara@gmail.com>
2015-06-20 16:12:41 +0200

    fixed script

--- data_prep/extract_best_itg.sh
+++ data_prep/extract_best_itg.sh
@@ -3,7 +3,8 @@
 echo "========================="
 echo "    Extract best ITG    "
 echo "========================"
-python ./itg-parse.py ${DATA_FOLDER}/itg.en ${DATA_FOLDER}/training.en > ${DATA_FOLDER}/itg.forest.en
+python ./itg-parse.py ${DATA_FOLDER}/training.en -g ${DATA_FOLDER}/itg.en -c ${DATA_FOLDER}/caches -b 1
+
 
 # get the forest
 # get the best tree
