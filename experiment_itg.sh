27538059f40f5c5717192581c42895e7ea87d8ac
finde <finde.xumara@gmail.com>
2015-06-23 19:06:58 +0200

    experiment script

--- experiment_itg.sh
+++ experiment_itg.sh
@@ -10,14 +10,12 @@ echo "experiment with language ${LANG} on ${N} lines"
 #mkdir data/caches/${LANG}
 
 echo "=== creating short sample set ==="
-head -n ${N} data/training.tagged.${LANG} > data/training.tagged.${LANG}.${N}
-head -n ${N} data/training.en-de.gdfa > data/training.en-de.gdfa.${N}
 head -n ${N} data/training.tok.${LANG} > data/training.tok.${LANG}.${N}
 
 echo "=== reordering ==="
 python reordering.py \
     -t data/training.tok.${LANG}.${N} \
-    -s data/training.tagged.${LANG}.${N} \
+    -s data/training.tok.${LANG}.${N} \
     -a data/training.en-de.gdfa.${N} \
     -g data/itg.${LANG} \
     -c data/caches/${LANG} \
--- experiment_random.sh
+++ experiment_random.sh
@@ -6,18 +6,17 @@ J=$3
 
 echo "experiment with language ${LANG} on ${N} lines"
 
-rm -rf data/caches/${LANG}.${N} 2> /dev/null
-mkdir data/caches/${LANG}.${N}
+#rm -rf data/caches/${LANG}.${N} 2> /dev/null
+#mkdir data/caches/${LANG}
 
 echo "=== creating short sample set ==="
-head -n ${N} data/training.tagged.${LANG} > data/training.tagged.${LANG}.${N}
-head -n ${N} data/training.en-de.gdfa > data/training.en-de.gdfa.${N}
 head -n ${N} data/training.tok.${LANG} > data/training.tok.${LANG}.${N}
 
+echo "=== reordering ==="
 python reordering.py \
     -t data/training.tok.${LANG}.${N} \
-    -s data/training.tagged.${LANG}.${N} \
+    -s data/training.tok.${LANG}.${N} \
     -a data/training.en-de.gdfa.${N} \
     -g data/itg.${LANG} \
-    -c data/caches/${LANG}.${N} \
+    -c data/caches/${LANG} \
     -j ${J}
